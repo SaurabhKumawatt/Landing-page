25c41fdbff57c93ff4c880d6120c040990bf2339
saurabhkumawatt <saurabhkumawat8959@gmail.com>
2025-10-02 10:02:00 +0530

    error fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+  base: '/',   // 👈 root se serve karo
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+})
